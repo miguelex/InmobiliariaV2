78c3990ed85016ceb1d99db8123cee8317285b80
migue <miguelexd@hotmail.com>
2020-06-09 12:46:49 +0200

    ultima actualizacion

--- src/componentes/vistas/ListaUsuarios.js
+++ src/componentes/vistas/ListaUsuarios.js
@@ -1,22 +1,23 @@
 import React, {useState, useEffect} from 'react';
-import {Container, Paper, Grid, Table, TableBody, TableRow, TableCell, Button, Dialog, DialogTitle, DialogContent, Select, MenuItem, DialogActions} from '@material-ui/core';
-import { useSelector, useDispatch } from 'react-redux';
-import { obtenerUsuariosApp, actualizarRoles } from '../../redux/actions/usuarioAction';
-import { enviarCorreoElectronico } from '../../redux/actions/emailAction';
-import { useStateValue } from '../../sesion/store';
-import { openMensajePantalla } from '../../sesion/actions/snackbarAction';
+import {Container, Paper, Grid, Table, TableBody, Button, TableCell, TableRow, Dialog, DialogTitle, DialogContent, Select, MenuItem, DialogActions} from "@material-ui/core";
+import {useSelector, useDispatch} from 'react-redux';
+import {obtenerUsuariosApp, actualizarRoles} from '../../redux/actions/usuarioAction';
+import {enviarCorreoElectronico} from '../../redux/actions/emailAction';
+
+import {useStateValue} from "../../sesion/store";
+import {openMensajePantalla} from  '../../sesion/actions/snackbarAction';
 import { refrescarSesion } from '../../sesion/actions/sesionAction';
-import {consumerFirebase} from '../../server/';
-import { enviarNotification } from '../../sesion/actions/notificationAction';
+import {consumerFirebase} from '../../server';
+import {enviarNotification} from '../../sesion/actions/notificationAction';
 
 const style = {
     paper : {
-        marginTop : 8,
-        display : "flex",
+        marginTop: 8,
+        display: "flex",
         flexDirection : "column",
-        alignItem : "center",
-        padding : "20px",
-        background : "#f5f5f5"
+        alignItems : "center",
+        padding: "20px",
+        backgroundColor: "#f5f5f5"
     },
     container : {
         paddingTop : "8px"
@@ -24,7 +25,6 @@ const style = {
 }
 
 const ListaUsuarios = props => {
-
     const [{sesion}, dispatch] = useStateValue();
 
     const [isLoading, setIsLoading] = useState(false);
@@ -37,12 +37,13 @@ const ListaUsuarios = props => {
     })
 
     const [selectRole, cambiarSelectRole] = useState("0");
-    
+
+
     const listaArreglo = useSelector(state => state.usuarioRedux.usuarios);
     const dispatchRedux = useDispatch();
-
-    useEffect(() => {
-        async function obtenerData() {
+    
+    useEffect( () => {
+        async function obtenerData(){
             await obtenerUsuariosApp(dispatchRedux);
         }
 
@@ -50,154 +51,219 @@ const ListaUsuarios = props => {
             setIsLoading(true);
             obtenerData();
         }
+
     })
 
-    const enviarEmail = (email) => {
-
+    const enviarEmail = (email) =>{
         const obj = {
-            email: email,
+            email : email,
             titulo : "Mensaje desde App React Extremo",
-            mensaje : "Gracias por participar en el curso"
+            mensaje : "Gracias por participar del curso"
         }
-        enviarCorreoElectronico(obj).then(dataServer => {
+
+        enviarCorreoElectronico(obj).then(dataServer=>{
             console.log(dataServer);
             openMensajePantalla(dispatch, {
                 open : true,
-                mensaje : " Se envio el correo electronico al destinatario " + email
+                mensaje: "Se envio el correo electronico al destinatario " + email
             })
+
         })
+
     }
 
     const abrirDialogConUsuario = row => {
         llenarUsuarioDialog(row);
         abreDialog(true);
-
     }
 
-    const eventoEnCombobox = event =>{
+    const eventoEnCombobox = event => {
         cambiarSelectRole(event.target.value);
     }
 
-    const agregarRol = async() => {
+    const agregarRol = async() =>{
+        
+        if(selectRole === "0"){
+            openMensajePantalla(dispatch, {
+                open: true,
+                mensaje:"Seleccione un role valido"
+            })
+            return;
+        }
+        
         if(!usuarioDialog.roles){
-            usuarioDialog.roles =[];
+            usuarioDialog.roles = [];
         }
 
-        const existeRole = usuarioDialog.roles.filter(rol => rol.nombre === selectRole)
-
-        if(existeRole.length === 0) {
-            // Authentication firebase
-            //Custom claims
-
+        let existeRole = usuarioDialog.roles.filter( rol => rol.nombre === selectRole)
+                
+        if(existeRole.length===0){
+            /***CREACION DEL  CUSTOM USER CLAIM */
             const customClaims = {};
-            usuarioDialog.roles.map(_role => {
+            usuarioDialog.roles.map(_role=>{
                 Object.defineProperty(customClaims, _role.nombre, {
-                    value: _role.estado,
-                    writable : true,
-                    enumerable : true,
+                    value : _role.estado,
+                    writable: true,
+                    enumerable: true,
                     configurable: true
-                })
+                } )
             })
 
             Object.defineProperty(customClaims, selectRole, {
                 value : true,
-                writable : true,
-                enumerable : true,
+                writable: true,
+                enumerable: true,
                 configurable: true
             })
+            /**FIN DE LA CREACION DEL CUSTOM USER CLAIM*/ 
+         
+            usuarioDialog.roles.push({nombre:selectRole, estado: true})
 
-            // Firestore
-            //Collection("Users")
 
-            usuarioDialog.roles.push({nombre : selectRole, estado : true})
+            
 
-            const respuesta = await actualizarRoles(dispatchRedux, usuarioDialog, customClaims);
-            console.log(respuesta);
-            obtenerUsuariosApp(dispatchRedux)
+            actualizarRoles(dispatchRedux, usuarioDialog, customClaims, props.firebase)
+            .then(respuesta => {
+                if(respuesta.data.status !== 'error'){
+                    refrescarSesion(props.firebase);
+        
+                    openMensajePantalla(dispatch, {
+                        open: true,
+                        mensaje:"Se guardo exitosamente el rol de usuario"
+                    })
+                }else{
+                    const rolesOriginal = usuarioDialog.roles.filter(rol => rol.nombre !== selectRole);
+                    llenarUsuarioDialog({
+                        ...usuarioDialog,
+                        roles : rolesOriginal
+                    });
+                    
+                    openMensajePantalla(dispatch, {
+                        open: true,
+                        mensaje: respuesta.data.mensaje
+                    })
+                }
 
-            refrescarSesion(props.firebase);
-
-            openMensajePantalla(dispatch, {
-                open : true,
-                mensaje : "Se guardo el rol de usuario"
+                obtenerUsuariosApp(dispatchRedux);
+                
             })
+            
+                    
+            
+            
+            
+            
+                
+            
+            
+
         }
+
+
     }
+
 
     const removerRol = async rol => {
 
-        const nuevoArregloRoles = usuarioDialog.roles.filter(currentRol => currentRol.nombre !== rol);
+        const usuarioRolesOriginal = usuarioDialog.roles;
+
+        const nuevoArregloRoles= usuarioDialog.roles.filter(currentRol => currentRol.nombre !== rol);
 
         usuarioDialog.roles = nuevoArregloRoles;
 
         const customClaims = {};
         nuevoArregloRoles.map(_rol => {
             Object.defineProperty(customClaims, _rol.nombre, {
-                value : _rol.nombre,
-                writable : true,
-                enumerable : true,
-                configurable : true
+                value: _rol.estado,
+                writable: true,
+                enumerable: true,
+                configurable: true
             })
         })
 
-        Object.defineProperty (customClaims, rol, {
+        Object.defineProperty(customClaims, rol,{
             value : false,
-            writable : true,
-            enumerable : true,
-            configurable : true
+            writable: true,
+            enumerable: true,
+            configurable: true
         })
 
-        const respuesta = actualizarRoles(dispatchRedux, usuarioDialog, customClaims)
-        console.log(respuesta);
+        actualizarRoles(dispatchRedux, usuarioDialog, customClaims, props.firebase)
+        .then(respuesta => {
+            
+            if(respuesta.data.status !== 'error'){
+                refrescarSesion(props.firebase);
+    
+                openMensajePantalla(dispatch, {
+                    open: true,
+                    mensaje:"Se guardo exitosamente el rol de usuario"
+                })
+            }else{
+                llenarUsuarioDialog({
+                    ...usuarioDialog,
+                    roles : usuarioRolesOriginal
+                })
+                openMensajePantalla(dispatch, {
+                    open: true,
+                    mensaje:respuesta.data.mensaje
+                })
+            }
 
-        obtenerUsuariosApp(dispatchRedux);
-        refrescarSesion(props.firebase);
+            obtenerUsuariosApp(dispatchRedux);
 
-        openMensajePantalla(dispatch, {
-            open : true,
-            mensaje :" Se elimino el rol seleccionado"
+            
         })
+        
+            
+        
+        
+
     }
 
+
     const enviarPushNotification = usuarioFila => {
+
         if(props.firebase.messagingValidation.isSupported()){
             const listaToken = usuarioFila.tokenArreglo;
             const obj = {
-                token: listaToken || []
+                token : listaToken || []
             }
 
-            enviarNotification(obj).then(respuestaServidor =>{
-                openMensajePantalla(dispatch, {
+            enviarNotification(obj).then(respuestaServidor => {
+                openMensajePantalla(dispatch,{
                     open: true,
                     mensaje: respuestaServidor.data.mensaje
                 })
             })
-        } else {
+
+        }else{
             openMensajePantalla(dispatch, {
                 open: true,
-                mensaje: " Esta version del navegador no soporta push notification"
+                mensaje : "Esta version de browser no soporta push notifications"
             })
         }
+
     }
+
+
 
     return (
         <Container style={style.container}>
-
-            <Dialog open = {estadoDialog} onClose={() => {abreDialog(false)}}>
+            <Dialog open={estadoDialog} onClose={()=> {abreDialog(false)}}>
                 <DialogTitle>
                     Roles del usuario {usuarioDialog.email || usuarioDialog.telefono}
                 </DialogTitle>
                 <DialogContent>
                     <Grid container justify="center">
                         <Grid item xs={6} sm={6}>
-                            <Select value ={selectRole} onChange={eventoEnCombobox}>
+                            <Select value={selectRole} onChange={eventoEnCombobox}>
                                 <MenuItem value={"0"}>Seleccione Rol</MenuItem>
                                 <MenuItem value={"ADMIN"}>Administrador</MenuItem>
                                 <MenuItem value={"OPERADOR"}>Operador</MenuItem>
                             </Select>
                         </Grid>
                         <Grid item xs={6} sm={6}>
-                            <Button color="secondary" variant="contained" onClick={() =>agregarRol()}>Agregar</Button>
+                            <Button color="secondary" onClick={()=>agregarRol()} variant="contained">Agregar</Button>
                         </Grid>
 
                         <Grid item xs={12} sm={12}>
@@ -206,24 +272,26 @@ const ListaUsuarios = props => {
                                     {usuarioDialog.roles
                                     ? usuarioDialog.roles.map((role, i) =>(
                                         <TableRow key={i}>
-                                            <TableCell align="left">{role.nombre}</TableCell>
-                                            <TableCell align="left">
-                                                <Button
-                                                    variant="contained"
-                                                    color="primary"
-                                                    size="small"
-                                                    onClick={()=> removerRol(role.nombre)}
-                                                >
-                                                    Eliminar
-                                                </Button>
-                                            </TableCell>
+                                                <TableCell aligh="left">{role.nombre}</TableCell>
+                                                <TableCell aligh="left">
+                                                    <Button
+                                                        variant="contained"
+                                                        color="primary"
+                                                        size="small"
+                                                        onClick ={()=> removerRol(role.nombre)}
+                                                        >
+                                                        Eliminar
+                                                    </Button>
+                                                </TableCell>
                                         </TableRow>
                                     ))
-                                    : null
+                                    :null
                                     }
                                 </TableBody>
                             </Table>
                         </Grid>
+
+
                     </Grid>
                 </DialogContent>
                 <DialogActions>
@@ -231,40 +299,46 @@ const ListaUsuarios = props => {
                 </DialogActions>
             </Dialog>
 
+            
             <Paper style={style.paper}>
                 <Grid container justify="center">
                     <Grid item xs={12} sm={12}>
                         <Table>
                             <TableBody>
                                 {
-                                    listaArreglo 
-                                    ? listaArreglo.map((row, i) => (
-                                        <TableRow key={i}>
+                                 listaArreglo 
+                                 ?  listaArreglo.map( (row,i) => (
+                                        <TableRow key = {i}>
                                             <TableCell align="left">{row.email || row.telefono}</TableCell>
-                                            <TableCell align="left">{row.nombre ? (row.nombre + ' ' + row.apellido) : 'No tiene nombre'}</TableCell>
+                                            <TableCell align="left">{ row.nombre ? ( row.nombre + ' ' + row.apellido) : 'No tiene nombre' }</TableCell>
                                             <TableCell>
-                                                <Button variant="contained" color="primary" size="small" onClick={() =>abrirDialogConUsuario(row)}>Roles</Button>
+                                                <Button variant="contained" onClick={()=>abrirDialogConUsuario(row)}  color="primary" size="small">Roles</Button>
                                             </TableCell>
+
+
                                             <TableCell>
-                                                <Button variant="contained" onClick={() => enviarPushNotification(row)} color="primary" size="small">Notificacion</Button>
+                                                <Button variant="contained" onClick={() => enviarPushNotification(row)} 
+                                                color="primary" size="small">Notificacion</Button>
                                             </TableCell>
+
+
+
                                             <TableCell>
-                                                <Button variant="contained" onClick={() => enviarEmail(row.email)} color="primary" size="small">Enviar Mensaje</Button>
+                                                <Button variant="contained" onClick={() => enviarEmail(row.email)} 
+                                                color="primary" size="small">Enviar Mensaje</Button>
                                             </TableCell>
                                         </TableRow>
-                                        )
                                     )
-                                    : ""
+                                 ) 
+                                 : "" 
                                 }
                             </TableBody>
                         </Table>
                     </Grid>
                 </Grid>
             </Paper>
-
         </Container>
     )
-
 }
 
 export default consumerFirebase(ListaUsuarios);
--- src/redux/actions/usuarioAction.js
+++ src/redux/actions/usuarioAction.js
@@ -13,21 +13,35 @@ export const obtenerUsuariosApp = (dispatch) => {
     })
 }
 
-export const actualizarRoles = (dispatch, usuario, role) => {
-    return new Promise (async (resolve, eject) => {
+export const actualizarRoles = (dispatch, usuario, role, firebase) => {
+    return new Promise((resolve, eject) => {
+      
+         firebase.auth.onAuthStateChanged(user=>{
+             if(user){
+                 user.getIdToken()
+                 .then(async tokenUsuario =>{
+                      
+                      const headers = {
+                          "Content-Type": "Application/json",
+                          "authorization" : "Bearer " + tokenUsuario
+                      }
+  
+                      const params = {
+                          id : usuario.id,
+                          role : role,
+                          roles : usuario.roles
+                      }
+  
+                      const respuesta = await axios.post('https://us-central1-templatecurso-6d079.cloudfunctions.net/usuariosMantenimiento',params, {"headers": headers});
+                      
+                      
+  
+                      resolve(respuesta);
+  
+                 })
+             }
+         })
         
-        const params = {
-            id : usuario.id,
-            role : role,
-            roles : usuario.roles
-        }
-        const dataRest = await axios.post('https://us-central1-templatecurso-6d079.cloudfunctions.net/usuariosMantenimiento', params);
-
-        dispatch ({
-            type : "ACTUALIZAR_ROLES",
-            payload : dataRest.data
-        })
-
-        resolve();
-    })
-}
+      
+    });
+  };
